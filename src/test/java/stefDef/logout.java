be134aefe589b09be8f113b4b731197495077eb0
apiani24 <51118477+apiani24@users.noreply.github.com>
2023-10-22 21:38:45 +0700

    Update logout.java

--- src/test/java/stefDef/logout.java
+++ src/test/java/stefDef/logout.java
@@ -1,2 +1,15 @@
-package stefDef;public class logout {
+package TestRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/logout.feature",
+        glue = "stefDef",
+        plugin = {"html:target/HTML_report.html"},
+        tags = "@LogoutSauceDemo"
+)
+public class logoutRunner {
 }
