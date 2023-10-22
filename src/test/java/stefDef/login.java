1a0c651c8348e266031e958bc11482534b660e2b
apiani24 <51118477+apiani24@users.noreply.github.com>
2023-10-22 21:38:10 +0700

    Update login.java

--- src/test/java/stefDef/login.java
+++ src/test/java/stefDef/login.java
@@ -1,2 +1,17 @@
-package stefDef;public class login {
+package TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stefDef",
+        plugin = {"html:target/HTML_report.html"},
+        tags = "@LoginSauceDemo"
+)
+
+public class loginRunner {
 }
